2ad840f4b45e0212fbf58ccd2dc869ac0b55661e
MIchal334 <mihal30060@gmail.com>
2025-02-02 19:41:48 +0100

    feat-04 add inject by configureation

--- src/main/java/com/stockMarket/controller/adapter/outbound/JpaActionRepository.java
+++ src/main/java/com/stockMarket/controller/adapter/outbound/JpaActionRepository.java
@@ -3,10 +3,11 @@ package com.stockMarket.controller.adapter.outbound;
 import com.stockMarket.controller.adapter.dto.ActionEntity;
 import com.stockMarket.controller.adapter.dto.IncidentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface JpaActionRepository extends JpaRepository<ActionEntity, Long> {
     Optional<ActionEntity> findByActionName(String actionName);
 }
--- src/main/java/com/stockMarket/controller/configuration/AppConfig.java
+++ src/main/java/com/stockMarket/controller/configuration/AppConfig.java
@@ -1,10 +1,18 @@
 package com.stockMarket.controller.configuration;
 
+import com.stockMarket.controller.adapter.outbound.DBIncidentRepository;
+import com.stockMarket.controller.adapter.outbound.JpaActionRepository;
+import com.stockMarket.controller.adapter.outbound.JpaIncidentRepository;
+import com.stockMarket.controller.application.port.outbound.IncidentRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-
+    @Bean
+    IncidentRepository incidentRepository(JpaIncidentRepository jpaIncidentRepository, JpaActionRepository jpaActionRepository) {
+        return new DBIncidentRepository(jpaIncidentRepository, jpaActionRepository);
+    }
 
 }
