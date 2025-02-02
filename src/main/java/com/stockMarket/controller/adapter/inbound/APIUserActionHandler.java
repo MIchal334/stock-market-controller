55ee86852d6796975c2db9749e3e89ce3a953f10
MIchal334 <mihal30060@gmail.com>
2025-02-02 12:16:03 +0100

    feat-03 implement APIUserActionHandler

---
+++ src/main/java/com/stockMarket/controller/adapter/inbound/APIUserActionHandler.java
@@ -0,0 +1,37 @@
+package com.stockMarket.controller.adapter.inbound;
+
+import com.stockMarket.controller.application.StockMarketService;
+import com.stockMarket.controller.domain.Incident;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/incident")
+class APIUserActionHandler {
+
+    private final StockMarketService stockMarketService;
+
+    APIUserActionHandler(StockMarketService stockMarketService) {
+        this.stockMarketService = stockMarketService;
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<List<Incident>> getAllIncidentByUserEmail(@PathVariable String email) {
+        return ResponseEntity.ok(this.stockMarketService.getAllIncidentByEmail(email));
+    }
+
+
+    public ResponseEntity<?> addNewIncident(Incident incident) {
+        this.stockMarketService.addNewIncident(incident);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
+    public ResponseEntity<?> deleteIncidentById(Long id) {
+        this.stockMarketService.removeIncidentById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
---
+++ src/main/java/com/stockMarket/controller/application/StockMarketService.java
@@ -0,0 +1,23 @@
+package com.stockMarket.controller.application;
+
+import com.stockMarket.controller.domain.Incident;
+
+import java.util.List;
+
+public class StockMarketService {
+
+    public void addNewIncident(Incident incident) {
+
+    }
+
+
+    public void removeIncidentById(Long id) {
+
+    }
+
+    public List<Incident> getAllIncidentByEmail(String email) {
+        return List.of();
+    }
+
+
+}
