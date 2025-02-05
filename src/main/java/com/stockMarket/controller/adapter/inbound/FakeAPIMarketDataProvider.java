55c20d5520f6eb871f0a2cd565dfadbddf6a68b1
MIchal334 <59032290+MIchal334@users.noreply.github.com>
2025-02-05 19:43:23 +0100

    feat-06 implement FakeAPIMarketDataProvider (#6)

---
+++ src/main/java/com/stockMarket/controller/adapter/inbound/FakeAPIMarketDataProvider.java
@@ -0,0 +1,32 @@
+package com.stockMarket.controller.adapter.inbound;
+
+import com.stockMarket.controller.application.port.inbound.MarketDataProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Map;
+
+public class FakeAPIMarketDataProvider implements MarketDataProvider {
+    @Value(value = "market.dataEndpoint")
+    private String endpoint;
+
+    private final WebClient webClient;
+
+    public FakeAPIMarketDataProvider(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+
+    @Override
+    public Map<String, Float> getActionPriceForCompanyList(List<String> companyList) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(endpoint).build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Float>>() {
+                })
+                .block();
+    }
+}
