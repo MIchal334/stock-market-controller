e0389acd13e2bc4ce41c3128e6896bb2d5db750a
MIchal334 <mihal30060@gmail.com>
2025-02-05 16:55:53 +0100

    feat-06 implement FakeAPIMarketDataProvider

---
+++ src/main/java/com/stockMarket/controller/adapter/inbound/WebClientConfig.java
@@ -0,0 +1,20 @@
+package com.stockMarket.controller.adapter.inbound;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Value(value = "market.address")
+    private String marketAddress;
+
+    @Bean
+    WebClient webClient(){
+        return WebClient.builder()
+                .baseUrl(marketAddress)
+                .build();
+    }
+
+}
--- src/main/java/com/stockMarket/controller/application/port/inbound/MarketDataProvider.java
+++ src/main/java/com/stockMarket/controller/application/port/inbound/MarketDataProvider.java
@@ -3,6 +3,6 @@ package com.stockMarket.controller.application.port.inbound;
 import java.util.List;
 import java.util.Map;
 
-interface MarketDataProvider {
+public interface MarketDataProvider {
     Map<String, Float> getActionPriceForCompanyList(List<String> companyList);
 }
